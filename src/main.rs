0c21c5416d195b0cd63e28ade6a799dc1c879557
Gregor Peach <gregorpeach@gmail.com>
2019-09-16 15:49:36 -0400

    Introduce a global router, and add basic routing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::env;
 
 mod request_handler;
@@ -6,5 +9,8 @@ mod server;
 fn main() {
     let development_mode = env::args().any(|arg| arg == "--development");
 
-    server::start_server(development_mode, request_handler::request_entrypoint)
+    // We don't want to hang the first REST call initializing our GLOBAL_HANDLER, so pre-initialize it
+    lazy_static::initialize(&request_handler::GLOBAL_HANDLER);
+
+    server::start_server(development_mode, request_handler::global_request_entrypoint)
 }
--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,7 +1,45 @@
 use hyper::{Body, Request, Response};
 
-pub fn request_entrypoint(req: Request<Body>) -> Response<Body> {
-    Response::new(Body::from(req.uri().path().to_string()))
+lazy_static! {
+    pub static ref GLOBAL_HANDLER: HttpRequestHandler = HttpRequestHandler::new();
+}
+
+pub fn global_request_entrypoint(req: Request<Body>) -> Response<Body> {
+    GLOBAL_HANDLER.handle(req)
+}
+
+#[derive(Debug)]
+pub struct HttpRequestHandler {}
+
+impl HttpRequestHandler {
+    fn new() -> HttpRequestHandler {
+        HttpRequestHandler {}
+    }
+
+    fn handle(&self, req: Request<Body>) -> Response<Body> {
+        let method = req.method();
+        let uri = req.uri();
+        let query = uri.query().unwrap_or("");
+
+        // Get the uri path, and then split it around slashes into components
+        let path_components: Vec<&str> = uri.path().split('/').skip(1).collect();
+
+        let response_body = if path_components[0] == "meta" {
+            format!(
+                "This is a meta request. path = {:?}, method = {:?}, query = {:?}",
+                path_components, method, query
+            )
+        } else if path_components[0] == "sl" {
+            format!(
+                "This is a serverless request. path = {:?}, method = {:?}, query = {:?}",
+                path_components, method, query
+            )
+        } else {
+            "Invalid request".to_string()
+        };
+
+        Response::new(Body::from(response_body))
+    }
 }
 
 #[cfg(test)]
@@ -11,7 +49,7 @@ mod tests {
     #[test]
     fn root_request_succeeds() {
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
-        let resp = request_entrypoint(req);
+        let resp = global_request_entrypoint(req);
 
         assert_eq!(resp.status().as_u16(), 200)
     }
