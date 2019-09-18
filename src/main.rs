f13917ed75a3177193b6f4be6761c5cb80afe3fc
Gregor Peach <gregorpeach@gmail.com>
2019-09-18 17:27:12 -0400

    Implement basic request parsing and routing
    With this iteration, requests are now routed to the right place
    in the code. Also the JSON request/responses are parsed and
    serialized acording to the spec.
    
    Lots left to do obviously, but now the meat of the work is the
    novel implementation -- not boilerplate.
    
    @Pat you may want to read over this to see how hyper works.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 #[macro_use]
+extern crate failure;
+#[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate serde;
 
 use std::env;
 
+mod components;
+mod error;
+mod model;
 mod request_handler;
 mod server;
 
 fn main() {
+    // Initialize logging
+    flexi_logger::Logger::with_str("debug, tokio_reactor=info, hyper=info")
+        .start()
+        .unwrap();
+    info!("worker starting... (logging initialized)");
+
+    // Parse command line arguments
     let development_mode = env::args().any(|arg| arg == "--development");
+    if development_mode {
+        info!("running in development mode");
+    }
 
     // We don't want to hang the first REST call initializing our GLOBAL_HANDLER, so pre-initialize it
     lazy_static::initialize(&request_handler::GLOBAL_HANDLER);
 
-    server::start_server(development_mode, request_handler::global_request_entrypoint)
+    server::start_server(development_mode, request_handler::global_request_entrypoint);
+
+    info!("Sever loop finished, shutting down...");
 }
